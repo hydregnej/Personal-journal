10985d64ed5d5ba184dfe77b84e6f03b0dcc6a03
hydregnej <78560174+hydregnej@users.noreply.github.com>
2024-06-27 14:46:42 +0500

    Теперь дата ставится текущая и она в правильном формате

--- src/components/JournalItem/JournalItem.jsx
+++ src/components/JournalItem/JournalItem.jsx
@@ -1,12 +1,13 @@
 import './JournalItem.css';
 
 function JournalItem({title, date, text}) {
+  const formatDate = new Intl.DateTimeFormat('ru-Ru').format(date)
 
 	return (
 		<div className='personal-item'>
 			<h2 className="personal-item__title">{title}</h2>
 			<div className="personal-item__date-and-text">
-				<p className="personal-item__date">{date}</p>
+				<p className="personal-item__date">{formatDate}</p>
 				<p className="personal-item__text">{text}</p>
 			</div>
 		</div>
